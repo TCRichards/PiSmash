9453bb93dbb9ac9d3ef7451b220467626ff1d27c
nickk124 <37184532+nickk124@users.noreply.github.com>
2019-07-17 16:12:28 -0400

    Created code for converting raw data string into objects usable by the page. Began work on bar graph callbacks

--- webpage/bokeh/bargraph.py
+++ webpage/bokeh/bargraph.py
@@ -9,7 +9,9 @@ import numpy as np
 
 output_file("bargraphSmash.html")
 
-players = ['BEEF', 'Tomato', 'postmabone', 'curt']
+
+#example data for testing only
+players = ['BEEF', 'Thomato', 'postmabone', 'curt']
 wincounts = [75, 56, 69, 24]
 
 src = ColumnDataSource(data=dict(players = players, wincounts = wincounts))
@@ -17,12 +19,34 @@ src = ColumnDataSource(data=dict(players = players, wincounts = wincounts))
 # sorting the bars means sorting the range factors
 sorted_players = sorted(players, key=lambda x: wincounts[players.index(x)])
 
-p = figure(x_range=sorted_players, plot_height=350, title="Win Counts per Player",
-           toolbar_location=None, tools="")
+p = figure(plot_height = 400, plot_width = 600, x_range=sorted_players, 
+           x_axis_label = 'player name',
+           y_axis_label = 'Win count',
+           title="Win count per player", tools = "xpan, xwheel_zoom",
+           active_scroll='xwheel_zoom', active_drag = "xpan")
 
 p.vbar(source = src, x='players', top='wincounts', width=0.9, fill_color=linear_cmap('wincounts', 'Viridis256', 0, max(wincounts)))
+
+# JS callbacks
+
+callback_plot = CustomJS(args=dict(src=src, p=p, axis=p.xaxis[0], x_range=p.x_range), code="""
+
+
+   
+""")
+
+
+# layout
+button_plot = Button(label = "Generate Plot", button_type = "primary")
+
+button_plot.js_on_click(callback_plot)
 
 p.xgrid.grid_line_color = None
 p.y_range.start = 0
 
-show(p)
+buttons = row(button_plot)
+
+layout = column(buttons, p)
+
+show(layout)
+save(layout)
