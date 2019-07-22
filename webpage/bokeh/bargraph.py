b711f7c9caeabd8862e3fb2159530fb59af6a5bb
nickk124 <37184532+nickk124@users.noreply.github.com>
2019-07-22 16:49:16 -0400

    Created scatter plot, and added mouseEnter plot loading

--- webpage/bokeh/bargraph.py
+++ webpage/bokeh/bargraph.py
@@ -5,14 +5,12 @@ from bokeh.models import CustomJS, ColumnDataSource, Slider, Button, BoxZoomTool
 from bokeh.plotting import Figure, output_file, show, save
 from bokeh.transform import linear_cmap
 from bokeh.models.tools import HoverTool
+from bokeh.palettes import Viridis256
+from bokeh.events import MouseEnter
 
 import numpy as np
 
 output_file("bargraphSmash.html")
-
-#example data for testing only
-#players = ['BEEF', 'Thomato', 'postmabone', 'curt', 'LONG']
-#counts = [75, 56, 69, 24, 101]
 
 #initial plot
 
@@ -22,19 +20,22 @@ counts = []
 src = ColumnDataSource(data=dict(players = players, counts = counts))
 
 p = Figure(plot_height = 400, plot_width = 600, x_range=players, 
-           x_axis_label = 'player name',
+           x_axis_label = 'Player name',
            y_axis_label = '',
-           title="Stats per player", tools = "xpan, xwheel_zoom",
+           title="Stats Per Player", tools = "xpan, xwheel_zoom",
            active_scroll='xwheel_zoom', active_drag = "xpan")
 
-p.title.text_font_size = "30pt"
-p.xaxis.axis_label_text_font_size = "14pt"
-p.yaxis.axis_label_text_font_size = "14pt"
+p.title.text_font_size = "18pt"
+p.xaxis.axis_label_text_font_size = "12pt"
+p.yaxis.axis_label_text_font_size = "12pt"
 
-try:
-    p.vbar(source = src, x='players', top='counts', width=0.9, fill_color=linear_cmap('counts', 'Viridis256', 0, max(counts)))
-except ValueError: #this just happens because on page load, data isn't loaded so the plotting gets grumpy
-    p.vbar(source = src, x='players', top='counts', width=0.9, fill_color=linear_cmap('counts', 'Viridis256', 0, 0))
+
+#try:
+#    p.vbar(source = src, x='players', top='counts', width=0.9, fill_color=linear_cmap('counts', 'Viridis256', 0, max(counts)))
+#except ValueError: #this just happens because on page load, data isn't loaded so the plotting gets grumpy
+#    p.vbar(source = src, x='players', top='counts', width=0.9, fill_color=linear_cmap('counts', 'Viridis256', 0, 0))
+
+p.vbar(source = src, x='players', top='counts', width=0.9)
 
 # tools
 p.add_tools(HoverTool(tooltips=[("Player name", "@players"), ("Count", "@counts")]))
@@ -71,7 +72,7 @@ callbackWin = CustomJS(args=dict(src=src, p=p, x_range=p.x_range, axis=p.yaxis[0
     data['players'] = players;
     data['counts'] = counts;
 
-    axis.axis_label = "Win Count";
+    axis.axis_label = "Win count";
 
     src.change.emit();   
     p.change.emit();
@@ -103,28 +104,65 @@ callbackKO = CustomJS(args=dict(src=src, p=p, x_range=p.x_range, axis=p.yaxis[0]
     data['players'] = players;
     data['counts'] = counts;
 
-    axis.axis_label = "KO Count";
+    axis.axis_label = "KO count";
 
     src.change.emit();
     p.change.emit();   
 
 """)
 
+callbackDamPerc = CustomJS(args=dict(src=src, p=p, x_range=p.x_range, axis=p.yaxis[0]), code="""
 
-# layout
+    p.reset.emit();
 
-buttonWin = Button(label = "Show Win Count (Default)", button_type = "primary")
+    let result = createDamPercBars();
+    let players_result = result[0];
+    let damPercCounts_result = result[1];
+
+    x_range.factors = players_result;
+
+    var data = src.data;
+
+    data['players'] = [];
+    data['counts'] = [];
+
+    players = data['players']
+    counts = data['counts']
+
+    for (let i = 0; i < players_result.length; i++){
+        players[i] = players_result[i];
+        counts[i] = damPercCounts_result[i];
+    }
+
+    data['players'] = players;
+    data['counts'] = counts;
+
+    axis.axis_label = "Total damage percentage";
+
+    src.change.emit();
+    p.change.emit();   
+
+""")
+
+# layout & callback setup
+
+p.js_on_event(MouseEnter, callbackWin)
+
+buttonWin = Button(label = "Show Win Count (default)", button_type = "primary")
 buttonWin.js_on_click(callbackWin)
 
 buttonKO = Button(label = "Show KO Count", button_type = "primary")
 buttonKO.js_on_click(callbackKO)
 
+buttonDamPerc = Button(label = "Show Total Damage Percentages", button_type = "primary")
+buttonDamPerc.js_on_click(callbackDamPerc)
+
 p.xgrid.grid_line_color = None
 p.y_range.start = 0
 
-buttons = row(buttonWin, buttonKO)
+buttons = column(buttonWin, buttonKO, buttonDamPerc)
 
-layout = column(buttons, p)
+layout = row(buttons, p)
 
 #show(layout)
 save(layout)
