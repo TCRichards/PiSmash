fa0df1ff4197fd1e840f055ada13a45011548cfa
Thomas Richards <thomas.c.richards@outlook.com>
2019-07-28 10:38:37 +0200

    testing stream v0

--- PiCamera/streamRTSP.py
+++ PiCamera/streamRTSP.py
@@ -1,4 +1,13 @@
-import subprocess
-import os
+from subprocess import Popen
 
-print('hello world')
+Popen("""dd if=/dev/video0 bs=1M | cvlc -vvv stream:///dev/stdin --sout
+'#rtp{sdp=rtsp://:8554/}' --demux=h264""", shell=True)
+input('Press ENTER to quit')
+
+Popen("""cvlc -vvv v4l2c:///dev/video0:width=640:height=480:chroma=H264 --sout
+'#rtp{sdp=rtsp://:8554/}' --demux h264""", shell=True)
+input('Press ENTER to quit')
+
+Popen("""dd if=/dev/video0 bs=1M | cvlc -vvv stream:///dev/stdin --sout
+'#rtp{sdp=rtsp://:8554/}' --demux=h264""", shell=True)
+input('Press ENTER to quit')
