36d4ac29a6c4273ca3bcdb51b3b8c7fe6b06d0c4
Thomas Richards <thomas.c.richards@outlook.com>
2019-07-28 11:53:46 +0200

    modified streaming instructions and finalized streamRTSP.py

--- PiCamera/streamRTSP.py
+++ PiCamera/streamRTSP.py
@@ -1,35 +1,20 @@
 import subprocess
 
-# Popen("""dd if=/dev/video0 bs=1M | cvlc -vvv stream:///dev/stdin --sout '#rtp{sdp=rtsp://:8554/}' --demux=h264""", shell=False)
-# input('Press ENTER to quit')
-#
-# Popen("""cvlc -vvv v4l2c:///dev/video0:width=640:height=480:chroma=H264 --sout '#rtp{sdp=rtsp://:8554/}' --demux h264""", shell=False)
-# input('Press ENTER to quit')
-#
-# Popen("""dd if=/dev/video0 bs=1M | cvlc -vvv stream:///dev/stdin --sout '#rtp{sdp=rtsp://:8554/}' --demux=h264""", shell=False)
-# input('Press ENTER to quit')
-
 # create two files to hold the output and errors, respectively
-with open('PiCamera/out.txt', 'w+') as fout:
-    with open('PiCamera/err.txt', 'w+') as ferr:
-        firstCommand = subprocess.Popen(
-            ["""cvlc -vvv v4l2c:///dev/video0:width=640:height=480:chroma=H264 --sout '#rtp{sdp=rtsp://:8554/}' --demux h264"""],
-            stdout=fout, stderr=ferr, shell=True)
-        try:
-            firstCommand.communicate(timeout=5)
-        except subprocess.TimeoutExpired:
-            firstCommand.kill()
-            secondCommand = subprocess.Popen(
-                ["""dd if=/dev/video0 bs=1M | cvlc -vvv stream:///dev/stdin --sout '#rtp{sdp=rtsp://:8554/}' --demux=h264"""],
-                stdout=fout, stderr=ferr, shell=True)
-            secondCommand.communicate()  # Don't force this one to end
+with open('PiCamera/streamLogs.txt', 'w+') as logs:    # Errors (although everything from streaming is interpreted as an error)
+    firstCommand = subprocess.Popen(
+        ["""cvlc -vvv v4l2c:///dev/video0:width=640:height=480:chroma=H264 --sout '#rtp{sdp=rtsp://:8554/}' --demux h264"""],
+        stderr=logs, shell=True)
+    try:
+        firstCommand.communicate(timeout=5)  # Executes the command and times out after 5 secs
+    except subprocess.TimeoutExpired:   # Kill the first command after 5 secs and launch the second
+        firstCommand.kill()
+        secondCommand = subprocess.Popen(
+            ["""dd if=/dev/video0 bs=1M | cvlc -vvv stream:///dev/stdin --sout '#rtp{sdp=rtsp://:8554/}' --demux=h264"""],
+            stderr=logs, shell=True)
+        secondCommand.communicate()  # Don't force this one to end
 
-        # reset file to read from it
-        fout.seek(0)
-        # save output (if any) in variable
-        output = fout.read()
-
-        # reset file to read from it
-        ferr.seek(0)
-        # save errors (if any) in variable
-        errors = ferr.read()
+    # reset file to read from it
+    logs.seek(0)
+    # save all stream outputs
+    errors = logs.read()
