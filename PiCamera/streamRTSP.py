28d0f36715b908f83f16cc8412f774fc8ccfa8a6
Thomas Richards <thomas.c.richards@outlook.com>
2019-07-28 11:07:57 +0200

    testing stream v0.4

--- PiCamera/streamRTSP.py
+++ PiCamera/streamRTSP.py
@@ -1,5 +1,4 @@
 import subprocess
-import os
 
 # Popen("""dd if=/dev/video0 bs=1M | cvlc -vvv stream:///dev/stdin --sout '#rtp{sdp=rtsp://:8554/}' --demux=h264""", shell=False)
 # input('Press ENTER to quit')
@@ -13,10 +12,18 @@ import os
 # create two files to hold the output and errors, respectively
 with open('PiCamera/out.txt', 'w+') as fout:
     with open('PiCamera/err.txt', 'w+') as ferr:
-        out = subprocess.run(
-            ["""cvlc -vvv v4l2c:///dev/video0:width=640:height=480:chroma=H264 --sout '#rtp{sdp=rtsp://:8554/}' --demux h264""",
-             """dd if=/dev/video0 bs=1M | cvlc -vvv stream:///dev/stdin --sout '#rtp{sdp=rtsp://:8554/}' --demux=h264"""],
+        firstCommand = subprocess.Popen(
+            ["""cvlc -vvv v4l2c:///dev/video0:width=640:height=480:chroma=H264 --sout '#rtp{sdp=rtsp://:8554/}' --demux h264"""],
             stdout=fout, stderr=ferr, shell=True)
+        try:
+            firstCommand.communicate(timeout=5)
+        except subprocess.TimeoutExpired:
+            firstCommand.kill()
+            secondCommand = subprocess.Popen(
+                ["""dd if=/dev/video0 bs=1M | cvlc -vvv stream:///dev/stdin --sout '#rtp{sdp=rtsp://:8554/}' --demux=h264"""],
+                stdout=fout, stderr=ferr, shell=True)
+            secondCommand.communicate()  # Don't force this one to end
+
         # reset file to read from it
         fout.seek(0)
         # save output (if any) in variable
