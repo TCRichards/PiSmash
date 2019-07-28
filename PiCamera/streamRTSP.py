77666fb4d850372c07d414a2fb61ed1bf48e3a78
Thomas Richards <thomas.c.richards@outlook.com>
2019-07-28 10:39:42 +0200

    testing stream v0.1

--- PiCamera/streamRTSP.py
+++ PiCamera/streamRTSP.py
@@ -1,13 +1,10 @@
 from subprocess import Popen
 
-Popen("""dd if=/dev/video0 bs=1M | cvlc -vvv stream:///dev/stdin --sout
-'#rtp{sdp=rtsp://:8554/}' --demux=h264""", shell=True)
+Popen("""dd if=/dev/video0 bs=1M | cvlc -vvv stream:///dev/stdin --sout '#rtp{sdp=rtsp://:8554/}' --demux=h264""", shell=True)
 input('Press ENTER to quit')
 
-Popen("""cvlc -vvv v4l2c:///dev/video0:width=640:height=480:chroma=H264 --sout
-'#rtp{sdp=rtsp://:8554/}' --demux h264""", shell=True)
+Popen("""cvlc -vvv v4l2c:///dev/video0:width=640:height=480:chroma=H264 --sout '#rtp{sdp=rtsp://:8554/}' --demux h264""", shell=True)
 input('Press ENTER to quit')
 
-Popen("""dd if=/dev/video0 bs=1M | cvlc -vvv stream:///dev/stdin --sout
-'#rtp{sdp=rtsp://:8554/}' --demux=h264""", shell=True)
+Popen("""dd if=/dev/video0 bs=1M | cvlc -vvv stream:///dev/stdin --sout '#rtp{sdp=rtsp://:8554/}' --demux=h264""", shell=True)
 input('Press ENTER to quit')
