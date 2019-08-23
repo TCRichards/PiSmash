75f533057efb6c119d68f889e4e542944a474ecc
Thomas <Thomas.c.richards@outlook.com>
2019-08-22 20:29:26 -0400

    Stashing local changes

--- PiCamera/gatherTrainingData.py
+++ PiCamera/gatherTrainingData.py
@@ -1,17 +1,10 @@
 import os
 import picamera
-from driveUpload import upload
 import datetime
 import time
 import socket
 import errno
-
-
-curDir = '/home/pi/PiSmash/PiCamera/'
-targetDir = os.path.join(curDir, 'trainingData')
-maxCapacity = 100   # Total number of
-total = 1e4         # Max out at 10,000 images
-delay = 0.5         # Time between consecutive images
+import sys
 
 
 def hasWifi():
@@ -28,35 +21,49 @@ def hasWifi():
 # Look for a wifi connection for up to 10 seconds
 startTime = time.time()
 foundWifi = False
-while time.time() < startTime + 10:
+while time.time() < startTime + 30:
     foundWifi = hasWifi()
     if foundWifi:
         break
 
-if foundWifi:   # Only begin the stream if wifi is detected
-    # Make the new directory if necessary
-    try:
-        os.mkdir(targetDir)
-    except OSError as e:
-        if e.errno == errno.EEXIST:
-            print('Directory not created.')
-    else:
-        raise
+# If we haven't found wifi after 30 seconds, give up
+if not foundWifi:
+    sys.exit(1)
 
-    # Setup the camera so that it auto-closes when finished
-    with picamera.PiCamera() as camera:
-        camera.resolution = (1280, 720)  # Standard HD resolution
-        i = 0
-        while i < total:
-            picNum = i % maxCapacity    # Modulo everything by the maximum capacity to only keep that many most recent images
-            if picNum == 0:
-                for img in os.listdir(targetDir):
-                    os.remove(os.path.join(targetDir, img))
+# Needed to wait until connected to wifi in order to import
+from driveUpload import upload
 
-            imageName = datetime.datetime.now().strftime('pic_%m%d-%H%M%S_{}.png'.format(picNum))
 
-            pathToImage = os.path.join(targetDir, imageName)
-            time.sleep(delay)   # Time (s) between consecutive pictures
-            camera.capture(pathToImage)
-            upload(pathToImage)
-            i += 1
+curDir = '/home/pi/PiSmash/PiCamera/'
+targetDir = os.path.join(curDir, 'trainingData')
+maxCapacity = 100   # Total number of
+total = 1e4         # Max out at 10,000 images
+delay = 0.5         # Time between consecutive images
+
+
+# Make the new directory if necessary
+try:
+    os.mkdir(targetDir)
+except OSError as e:
+    if e.errno == errno.EEXIST:
+        print('Directory not created.')
+else:
+    raise
+
+# Setup the camera so that it auto-closes when finished
+with picamera.PiCamera() as camera:
+    camera.resolution = (1280, 720)  # Standard HD resolution
+    i = 0
+    while i < total:
+        picNum = i % maxCapacity    # Modulo everything by the maximum capacity to only keep that many most recent images
+        if picNum == 0:
+            for img in os.listdir(targetDir):
+                os.remove(os.path.join(targetDir, img))
+
+        imageName = datetime.datetime.now().strftime('pic_%m%d-%H%M%S_{}.png'.format(picNum))
+
+        pathToImage = os.path.join(targetDir, imageName)
+        time.sleep(delay)   # Time (s) between consecutive pictures
+        camera.capture(pathToImage)
+        upload(pathToImage)
+        i += 1
